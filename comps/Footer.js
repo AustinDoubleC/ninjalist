2ffa22a7060e80c5da93de609ba00a95ff093b76
Austin <austinccy81@gmail.com>
2022-07-13 11:32:21 +0100

    first

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <footer>
+            <p>Copyright 2022 Austin</p>
+        </footer>
+     );
+}
+ 
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+const About = () => {
+    return ( 
+    <>
+        <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas"/>
+        </Head>
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent efficitur turpis sit amet porttitor aliquet. Proin sodales ornare placerat. Duis posuere est eu dignissim fermentum. Aliquam augue elit, lacinia id.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent efficitur turpis sit amet porttitor aliquet. Proin sodales ornare placerat. Duis posuere est eu dignissim fermentum. Aliquam augue elit, lacinia id.</p>
+        </div> 
+    </>
+    )
+}
+
+export default About;
